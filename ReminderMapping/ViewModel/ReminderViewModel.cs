df082b0e20d06ae65f22f2c8e714e658d3130403
naveenkumar.sanjeevirayan <naveenkumar.sanjeevirayan@syncfusion.com>
2020-12-16 10:00:51 +0530

    Added reminder samples

---
+++ ReminderMapping/ViewModel/ReminderViewModel.cs
@@ -0,0 +1,117 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ReminderMapping
+{
+    public class ReminderViewModel : NotificationObject
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReminderViewModel" /> class.
+        /// </summary>
+        public ReminderViewModel()
+        {
+            this.Events = new ObservableCollection<SchedulerModel>();
+            this.CreateSchedulerAppointments();
+            DisplayDate = DateTime.Now.Date.AddHours(9);
+        }
+
+        #endregion Constructor
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets event collection.
+        /// </summary>
+        public ObservableCollection<SchedulerModel> Events { get; set; }
+
+        /// <summary>
+        /// Gets or sets display date
+        /// </summary>
+        public DateTime DisplayDate { get; set; }
+
+        #endregion Properties
+
+        #region CreateSchedulerAppointmentCollection
+
+        /// <summary>
+        /// Method to create Scheduler appointment collection
+        /// </summary>
+        private void CreateSchedulerAppointments()
+        {
+            this.Events.Add(new SchedulerModel()
+            {
+                From = DateTime.Now,
+                To = DateTime.Now.AddHours(1),
+                Color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF339933")),
+                EventName = "Conference",
+                Reminders = new ObservableCollection<ReminderModel>
+                {
+                    new ReminderModel { TimeInterval = new TimeSpan(0)},
+                }
+            });
+
+            this.Events.Add(new SchedulerModel()
+            {
+                From = DateTime.Now.Date.AddDays(-2).AddHours(10),
+                To = DateTime.Now.Date.AddDays(-2).AddHours(11),
+                Color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFA2C139")),
+                EventName = "Business Meeting",
+                Reminders = new ObservableCollection<ReminderModel>
+                {
+                    new ReminderModel { TimeInterval = new TimeSpan(0, 15, 0)},
+                }
+            });
+
+            this.Events.Add(new SchedulerModel()
+            {
+                From = DateTime.Now.Date.AddDays(2).AddHours(12),
+                To = DateTime.Now.Date.AddDays(2).AddHours(13),
+                Color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00ABA9")),
+                EventName = "Medical check up",
+                Reminders = new ObservableCollection<ReminderModel>
+                {
+                    new ReminderModel { TimeInterval = new TimeSpan(1, 0, 0, 0)},
+                    new ReminderModel { TimeInterval = new TimeSpan(2, 0, 0, 0)},
+                    new ReminderModel { TimeInterval = new TimeSpan(3, 0, 0)},
+                }
+            });
+
+            this.Events.Add(new SchedulerModel()
+            {
+                From = DateTime.Now.Date.AddDays(-10).AddHours(15),
+                To = DateTime.Now.Date.AddDays(-10).AddHours(16),
+                Color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFE671B8")),
+                EventName = "Project Status Discussion",
+                Notes = "provides an opportunity to share information across the whole team.",
+                RecurrenceRule = "FREQ=WEEKLY;BYDAY=WE;INTERVAL=1;COUNT=20",
+                Reminders = new ObservableCollection<ReminderModel>
+                {
+                    new ReminderModel { TimeInterval = new TimeSpan(0, 15, 0)},
+                }
+            });
+
+            this.Events.Add(new SchedulerModel()
+            {
+                From = DateTime.Now.Date.AddDays(3).AddHours(14),
+                To = DateTime.Now.Date.AddDays(3).AddHours(15),
+                Color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD80073")),
+                EventName = "GoToMeeting",
+                Reminders = new ObservableCollection<ReminderModel>
+                {
+                    new ReminderModel { TimeInterval = new TimeSpan(0, 15, 0)},
+                }
+            });
+
+        }
+
+        #endregion CreateSchedulerAppointmentCollection
+    }
+}
---
+++ SchedulerReminder/ViewModel/ReminderViewModel.cs
@@ -0,0 +1,118 @@
+﻿using Syncfusion.UI.Xaml.Scheduler;
+using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Scheduler_Reminder
+{
+    public class ReminderViewModel : NotificationObject
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReminderViewModel" /> class.
+        /// </summary>
+        public ReminderViewModel()
+        {
+            this.Events = new ScheduleAppointmentCollection();
+            this.CreateSchedulerAppointments();
+            DisplayDate = DateTime.Now.Date.AddHours(9);
+        }
+
+        #endregion Constructor
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets event collection.
+        /// </summary>
+        public ScheduleAppointmentCollection Events { get; set; }
+
+        /// <summary>
+        /// Gets or sets display date
+        /// </summary>
+        public DateTime DisplayDate { get; set; }
+
+        #endregion Properties
+
+        #region CreateSchedulerAppointmentCollection
+
+        /// <summary>
+        /// Method to create Scheduler appointment collection
+        /// </summary>
+        private void CreateSchedulerAppointments()
+        {
+            this.Events.Add(new ScheduleAppointment()
+            {
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddHours(1),
+                AppointmentBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF339933")),
+                Subject = "Conference",
+                Reminders = new ObservableCollection<SchedulerReminder>
+                {
+                    new SchedulerReminder { ReminderTimeInterval = new TimeSpan(0)},
+                }
+            });
+
+            this.Events.Add(new ScheduleAppointment()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-2).AddHours(10),
+                EndTime = DateTime.Now.Date.AddDays(-2).AddHours(11),
+                AppointmentBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFA2C139")),
+                Subject = "Business Meeting",
+                Reminders = new ObservableCollection<SchedulerReminder>
+                {
+                    new SchedulerReminder { ReminderTimeInterval = new TimeSpan(0, 15, 0)},
+                }
+            });
+
+            this.Events.Add(new ScheduleAppointment()
+            {
+                StartTime = DateTime.Now.Date.AddDays(2).AddHours(12),
+                EndTime = DateTime.Now.Date.AddDays(2).AddHours(13),
+                AppointmentBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00ABA9")),
+                Subject = "Medical check up",
+                Reminders = new ObservableCollection<SchedulerReminder>
+                {
+                    new SchedulerReminder { ReminderTimeInterval = new TimeSpan(1, 0, 0, 0)},
+                    new SchedulerReminder { ReminderTimeInterval = new TimeSpan(2, 0, 0, 0)},
+                    new SchedulerReminder { ReminderTimeInterval = new TimeSpan(3, 0, 0)},
+                }
+            });
+
+            this.Events.Add(new ScheduleAppointment()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-10).AddHours(15),
+                EndTime = DateTime.Now.Date.AddDays(-10).AddHours(16),
+                AppointmentBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFE671B8")),
+                Subject = "Project Status Discussion",
+                Notes = "provides an opportunity to share information across the whole team.",
+                RecurrenceRule = "FREQ=WEEKLY;BYDAY=WE;INTERVAL=1;COUNT=20",
+                Reminders = new ObservableCollection<SchedulerReminder>
+                {
+                    new SchedulerReminder { ReminderTimeInterval = new TimeSpan(0, 15, 0)},
+                }
+            });
+
+            this.Events.Add(new ScheduleAppointment()
+            {
+                StartTime = DateTime.Now.Date.AddDays(3).AddHours(14),
+                EndTime = DateTime.Now.Date.AddDays(3).AddHours(15),
+                AppointmentBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD80073")),
+                Subject = "GoToMeeting",
+                Reminders = new ObservableCollection<SchedulerReminder>
+                {
+                    new SchedulerReminder { ReminderTimeInterval = new TimeSpan(0, 15, 0)},
+                }
+            });
+
+        }
+
+        #endregion CreateSchedulerAppointmentCollection
+    }
+}
